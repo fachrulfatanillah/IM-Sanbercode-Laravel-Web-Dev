3bab20018c818e7b89a435ac184bdc2b39b081d7
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-10-31 10:49:52 +0700

    Tugas 14 - Mengatur Database dengan Migration

--- inventoryapp_fachrurfatanillah/database/migrations/2025_10_31_032219_profile.php
+++ inventoryapp_fachrurfatanillah/database/migrations/2025_10_31_032219_profile.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profile', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ["staff", "admin"])->default('staff');
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users')->onDelete("cascade")->onUpdate("cascade");
+            $table->integer('age');
+            $table->text('bio');
+            $table->timestamps();
         });
     }
 
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profile');
     }
 };
--- inventoryapp_fachrurfatanillah/database/migrations/2025_10_31_032244_categories.php
+++ inventoryapp_fachrurfatanillah/database/migrations/2025_10_31_032244_categories.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ["staff", "admin"])->default('staff');
+            $table->text('description');
+            $table->timestamps();
         });
     }
 
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
